ffdfe64a1e54cf59e5dd04d6257cf56b761e3b33
Ubaid Chawla <uchawla96@gmail.com>
2018-12-08 12:01:09 +0500

    folder clean extra files removed

--- registerdoctor.php
+++ registerdoctor.php
@@ -12,18 +12,17 @@ echo("error in connection");
 }
 if(isset($_POST['submit'])){
 $name=$_POST['name'];
-$username=$_POST['username'];
-$password=$_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 $gender=$_POST['gender'];
 $email=$_POST['email'];
-$doctor_id=$_POST['doctor_id'];
+$doctor_id = $_POST['doctor_id'];
 $degree_name=$_POST['degree_name'];
-$degree_id=$_POST['degree_id'];
-$speciality_id=$_POST['speciality_id'];
+$degree_id = $_POST['degree_id'];
+$speciality_id = $_POST['speciality_id'];
 $speciality_name=$_POST['speciality_name'];
-
 $register_query = "INSERT INTO `doctor` (`name`, `username`, `password`, `gender`, `email`, `doctor_id`, `degree_name`, `degree_id`, `speciality_id`, `speciality_name`)
-								VALUES ('$name','$username','$password','$gender','$email','$doctor_id','$degree_name','$degree_id','$speciality_id','$speciality_name')";
+ VALUES ('$name','$username','$password','$gender','$email','$doctor_id' ,'$degree_name','$degree_id','$speciality_id','$speciality_name')";
 try{
 $register_result = mysqli_query($conn, $register_query);
 if($register_result){
@@ -43,121 +42,82 @@ echo("error".$ex->getMessage());
 <html>
 <head>
 <meta charset="utf-8">
-<title>Register a Doctor</title>
+<title>Untitled Document</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="src/css/custom.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
- 
-<body>
-<form action="" method="post">
-<table align="center">
-<tr>
-<td>Name:</td>
-<td><input type="text" name="name" placeholder="enter your first name"></td>
-</tr>
-<tr>
-<td>username</td>
-<td><input type="text" name="username" placeholder="enter username"></td>
-</tr>
-<tr>
-<td>password</td>
-<td><input type="password" name="password" placeholder="enter password"></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
 
-<td>Gender</td>
- 
- 
-<td><input type="radio" name="gender" value="Male">Male</td>
-</tr>
-<tr>
-<td></td>
-<td><input type="radio" name="gender" value="female">Female</td>
-</tr>
-<tr>
-<td></td>
-<td><input type="radio" name="gender" value="others">others</td>
-</tr>
-<tr>
-<td>email:</td>
-<td><input type="text" name="email" placeholder="enter your email"></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
- 
-<tr>
-<td>doctor_id:</td>
-<td><input type="number" name="doctor_id" placeholder="enter doc id"></td>
- 
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
- 
-<tr>
-<td>degree_name:</td>
-<td><input type="text" name="degree_name" placeholder="name your degree"></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-</tr>
- 
-<tr>
-<td>degree_id:</td>
-<td><input type="number" name="degree_id" placeholder="enter degree_id"></td>
-</tr>
-<tr>
-<td>speciality_id:</td>
-<td><input type="number" name="speciality_id" placeholder="enter speciality_id"></td>
-</tr>
-<tr>
-<td>speciality_name:</td>
-<td><input type="text" name="speciality_name" placeholder="enter speciality_name"></td>
-</tr>
- 
-<tr>
-<td></td>
-<td>  <button type="submit" name="submit">Register</button>
-</td>
-</tr>
-</table>
+<body style="background-color:#B6B6B4;">
+
+	<form style="max-width:500px;padding:3%;margin:auto;" action="" method="post">
+  <h2>Doctor Register Form</h2>
+
+    <div class="input-container">
+    <i class="fa fa-username icon"></i>
+    <input class="input-field" type="text" placeholder="Name" name="name">
+  </div>
+
+
+  <div class="input-container">
+    <i class="fa fa-username icon"></i>
+    <input class="input-field" type="text" placeholder="username" name="username">
+  </div>
+
+
+  
+  <div class="input-container">
+    <i class="fa fa-username icon"></i>
+    <input class="input-field" type="text" placeholder="password" name="password">
+  </div>
+
+  <div class="form-group">
+    <select style="width:100%;color:white;background:#AF2A2A;padding:5%;" name="gender">
+      <option>Gender</option>
+		<option	value="Male">Male</option>
+  		<option value="Female">Female</option>
+  		<option	value="Other">Other</option>`
+	   </select>
+  </div>
+
+
+  <div class="input-container">
+    <i class="fa fa-envelope icon"></i>
+    <input class="input-field" type="text" placeholder="Email" name="email">
+  </div>
+
+  <div class="input-container">
+    <i class="fa fa-briefcase icon"></i>
+    <input class="input-field" type="number" placeholder="Doctor_id" name="doctor_id">
+  </div>
+
+  <div class="input-container">
+    <i class="fa fa-username-md icon"></i>
+    <input class="input-field" type="text" placeholder="degree_name" name="degree_name">
+  </div>
+
+  <div class="input-container">
+    <i class="fa fa-doctor_id icon"></i>
+    <input class="input-field" type="number" placeholder="degree_id" name="degree_id">
+  </div>
+
+  <div class="input-container">
+    <i class="fa fa-key icon"></i>
+    <input class="input-field" type="text" placeholder="speciality_id" name="speciality_id">
+  </div>
+
+
+  <div class="input-container">
+    <i class="fa fa-key icon"></i>
+    <input class="input-field" type="text" placeholder="Speciality_name" name="speciality_name">
+  </div>
+  
+
+  <button type="submit" name="submit" class="btn">Register</button>
 </form>
+		
+	
 </body>
 </html>
